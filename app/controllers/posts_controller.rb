a6398650bf29bffa7d93c75b5f6f6749955a4973
Shane Schmaltz <stschmaltz@gmail.com>
2023-06-09 15:26:10 -0600

    add a show post page and edit post page. Make a real nav

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "posts", to: "posts#index"
+  root 'posts#index'
+
+  get 'posts', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  get 'posts/:id/edit', to: 'posts#edit', as: 'edit_post'
 end
