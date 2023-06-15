9e9fda47716f5a3fbffed7d8b5540cd9e2a2cd67
Shane Schmaltz <stschmaltz@gmail.com>
2023-06-15 16:56:54 -0600

    add user resource and implement user signup. Also refactor a bunch of styles and layout problems

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create]
   end
+
+  resources :users
+  get 'signup', to: 'users#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_211437) do
+ActiveRecord::Schema.define(version: 2023_06_15_212408) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2023_06_15_211437) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "published_at"
     t.text "tags"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "posts"
