1b1eca8d7fdae8816d34fbc7bf5ca20ae86f21c1
Shane Schmaltz <stschmaltz@gmail.com>
2023-06-19 19:22:32 -0600

    move tag filter logic to a view helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,28 @@ module PostsHelper
       'Not published yet'
     end
   end
+
+  def tag_filter_dropdown(filter)
+    @tags = Tag.all
+    form_with url: '#', id: 'tag_filter_form', method: :get, class: 'bg-red' do |form|
+      capture do
+        concat form.select :filter, options_for_select([['All Posts', '']] + @tags.map do |t|
+                                                                               [t.name, t.name]
+                                                                             end, filter), {}, { id: 'filter', class: 'form-input-selector' }
+      end
+    end
+  end
+
+  def tag_filter_javascript
+    javascript_tag <<~JS
+      document.getElementById('filter').addEventListener('change', function(event) {
+        var tag = event.target.value;
+        if(tag) {
+          window.location = '/posts/filter/' + tag;
+        } else {
+          window.location = '/posts';
+        }
+      });
+    JS
+  end
 end
